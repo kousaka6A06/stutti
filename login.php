f4b582782dab11f06ce7353b24cceccdec991742
kamei <kmyr3.0@gmail.com>
2024-10-31 12:48:35 +0900

    ユーザー周りのコントローラー

--- login.php
+++ login.php
@@ -4,25 +4,42 @@ require_once 'config/constants.php';
 require_once 'model/User.php';
 require_once 'utils/Utils.php';
 
-if (isset($_SESSION['user_id'])) {
+// セッションが存在しない場合
+if (session_status() === PHP_SESSION_NONE) {
+    // セッションを開始する
+    session_start();
+}
+
+// ログイン済みの場合
+if (isset($_SESSION['userId'])) {
+    // マイページ画面に遷移
     header('Location: ' . BASE_DOMAIN . '/mypage.php');
     exit;
 }
 
+// ヘッダーのログインボタンが押下された場合
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // ログイン画面を描画
     Utils::loadView('ログイン', 'view/v_login.php');
 
+// ログイン画面でログインボタンが押下された場合
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // ユーザーインスタンスを作成して画面で入力された内容をセット
     $user = new User();
     $user->login_id = $_POST['login-id'];
     $user->password = $_POST['password'];
 
+    // ログイン試行
+    // ログインに成功した場合
     if ($user->login()) {
-        $_SESSION['user_id'] = $user->id;
+        // セッションにユーザーIDを保存してマイページ画面に遷移
+        $_SESSION['userId'] = $user->id;
         header('Location: ' . BASE_DOMAIN . '/mypage.php');
 
+    // ログインに失敗した場合
     } else {
-        $error_message = 'ログインID かパスワードが異なります';
+        // エラーメッセージと共にログイン画面を再描画
+        $errorMessage = 'Stutti ID かパスワードが異なります';
         Utils::loadView('ログイン', 'view/v_login.php');
     }
 }
--- myPage.php
+++ myPage.php
@@ -1,11 +1,35 @@
 <?php
 
 require_once 'config/constants.php';
+require_once 'model/User.php';
 require_once 'utils/Utils.php';
 
-// if (!isset($_SESSION['user_id'])) {
-//     header('Location: ' . BASE_DOMAIN . '/index.php');
-//     exit;
-// }
+// セッションが存在しない場合
+if (session_status() === PHP_SESSION_NONE) {
+    // セッションを開始する
+    session_start();
+}
 
+// 未ログインの場合
+if (!isset($_SESSION['userId'])) {
+    // エラーメッセージと共にログイン画面に遷移
+    $errorMessage = 'ログインしてください';
+    header('Location: ' . BASE_DOMAIN . '/login.php');
+    exit;
+}
+
+// ユーザーインスタンスを作成してセッション情報をセット
+$user = new User();
+$user->id = $_SESSION['userId'];
+
+// TODO: [コントローラー]
+// ユーザー情報を取得する
+// $user = $user->getUserById();
+
+// TODO: [モデル]
+// getUserById():User
+// あらかじめプロパティに設定されたuserIdを使って、Userを検索して返却してください
+// PDOStatement::fetch() の引数にPDO::FETCH_CLASS を使うと良い感じかも。。
+
+// マイページ画面を描画
 Utils::loadView('マイページ', 'view/v_myPage.php');
