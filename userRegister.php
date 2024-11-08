b0fe7b3c20bf705aeb477d6e9e0952582b89a094
kamei <kmyr3.0@gmail.com>
2024-11-08 14:04:14 +0900

    画像アップロード周りのコントローラー

--- userRegister.php
+++ userRegister.php
@@ -16,7 +16,7 @@ $stuttiId = isset($_POST['stutti-id']) ? Utils::e($_POST['stutti-id']) : null;
 $password = isset($_POST['password']) ? Utils::e($_POST['password']) : null;
 $name = isset($_POST['name']) ? Utils::e($_POST['name']) : null;
 $mailAddress = isset($_POST['mail-address']) ? Utils::e($_POST['mail-address']) : null;
-$avatar = isset($_POST['avatar']) ? Utils::e($_POST['avatar']) : null;
+$avatar = isset($_POST['avatar']) ? $_POST['avatar'] : null;
 
 // ログイン済みの場合
 if ($userId) {
@@ -39,10 +39,48 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $user->setPassword($password);
     $user->setName($name);
     $user->setMailAddress($mailAddress);
-    if ($avatar) {
-        $user->setAvatar($avatar);
-    } else {
+
+    // アバター画像が画面から渡されなかった場合
+    if (!$avatar) {
         $user->setAvatar(DEFAULT_AVATAR);
+
+    // アバター画像が画面から渡された場合
+    } else {
+        // TODO: [コントローラー]
+        // 画像アップロード試行
+        // $res = $user->uploadAvatar($avatar);
+
+        // 画像アップロードに成功した場合
+        if (!is_int($res)) {
+
+        // TODO: [モデル]
+        // uploadAvatar($avatar):mixed
+        // 引数で渡されたアバター画像をアップロードしてください
+        // アップロードに成功した場合は、一意のファイル名を返却してください
+        // アップロードに失敗した場合は、エラーコード（constants.php参照）を返却してください
+
+            $user->setAvatar($res);
+
+        // 画像アップロードに失敗した場合
+        } else {
+            // エラーコードによってメッセージ内容を設定
+            $messages = [
+                ERR_CODE_INI_SIZE => 'アップロード画像の最大サイズ制限を超えています(PHP)',
+                ERR_CODE_FORM_SIZE => 'アップロード画像の最大サイズ制限を超えています(HTML)',
+                ERR_CODE_PARTIAL => 'ファイルが一部しかアップロードされていません。<br>繰り返し失敗する場合は管理者に連絡して下さい。',
+                ERR_CODE_NO_TMP_DIR => '一時保存フォルダが存在しないためアップロード処理を中止しました。<br>繰り返し失敗する場合は管理者に連絡して下さい。',
+                ERR_CODE_CANT_WRITE => 'ディスクへの書き込みに失敗しました。<br>繰り返し失敗する場合は管理者に連絡して下さい。',
+                ERR_CODE_MODULE => '拡張モジュールによってアップロードが中断されました。<br>繰り返し失敗する場合は管理者に連絡して下さい。',
+                ERR_CODE_EXTENSION => '画像以外のファイルはアップロードできません',
+                ERR_CODE_MIME_TYPE => 'ファイルの内容が画像ではありません',
+                ERR_CODE_FAIL_UPLOAD => 'アップロード処理に失敗しました。<br>繰り返し失敗する場合は管理者に連絡して下さい。'
+            ];
+
+            // セッションにメッセージを保存してユーザー登録画面を再描画
+            $_SESSION['message'] = $messages[$res];
+            Utils::loadView('ユーザー登録', 'view/v_userRegister.php');
+            exit;
+        }
     }
 
     // ユーザー登録試行
