fe2c28894f514dd411c6991d5d6e4fb02dece484
takaoka <momo7na10-miona@yahoo.co.jp>
2024-11-25 09:36:21 +0900

    最大文字数のバリデーション追加、コメント・メッセージの投稿日時表示の表示方法の変更

--- view/v_tutti.php
+++ view/v_tutti.php
@@ -82,7 +82,7 @@ global $tuttiInfo, $groupInfos, $commentInfos, $newsInfos;
                             </td>
                             <td class="p-3 w-75">
                                 <?= $commentInfo['content'] ?><br>
-                                <small><?= $commentInfo['created_at'] ?></small>
+                                <small class="opacity-50"><?= $commentInfo['created_at'] ?></small>
                             </td>
                         </tr>
                         <?php endforeach; ?>
@@ -90,7 +90,7 @@ global $tuttiInfo, $groupInfos, $commentInfos, $newsInfos;
                 </table>
                 <p class="text-center">コメントを投稿する</p>
                 <form action="commentPost.php" method="POST" enctype="multipart/form-data" class="text-center">
-                    <input type="text" name="name" class="ps-1 w-75" placeholder="投稿者（未入力の場合は「名無し」で投稿されます）">
+                    <input type="text" name="name" class="ps-1 w-75" max="250" placeholder="投稿者（未入力の場合は「名無し」で投稿されます）">
                     <textarea type="text" name="content" class="p-1 w-75" placeholder="投稿内容" required></textarea><br>
                     <input type="hidden" name="tid" value="<?= $tuttiInfo['id'] ?>">
                     <button type="submit" class="btn btn-dark w-30">投稿</button>
